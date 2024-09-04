e38b7871f5551174898f130ad0ec9ae5e976f102
98Adam <adam.magyari@yahoo.com>
2024-09-04 02:53:10 +0200

    Final Popup

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -47,7 +47,6 @@ function validNick() {
 }
 
 window.onload = function () {
-
     var btn = document.getElementById('startButton'),
         btnS = document.getElementById('spectateButton'),
         nickErrorText = document.querySelector('#startMenu .input-error');
@@ -57,8 +56,6 @@ window.onload = function () {
     };
 
     btn.onclick = function () {
-
-        // Checks if the nick is valid.
         if (validNick()) {
             nickErrorText.style.opacity = 0;
             startGame('player');
@@ -80,7 +77,6 @@ window.onload = function () {
 
     playerNameInput.addEventListener('keypress', function (e) {
         var key = e.which || e.keyCode;
-
         if (key === global.KEY_ENTER) {
             if (validNick()) {
                 nickErrorText.style.opacity = 0;
@@ -150,12 +146,11 @@ $("#split").click(function () {
 
 function handleDisconnect() {
     socket.close();
-    if (!global.kicked) { // We have a more specific error message 
+    if (!global.kicked) { // We have a more specific error message
         render.drawErrorMessage('Disconnected!', graph, global.screen);
     }
 }
 
-// socket stuff.
 function setupSocket(socket) {
     // Handle ping.
     socket.on('pongcheck', function () {
@@ -164,11 +159,9 @@ function setupSocket(socket) {
         window.chat.addSystemLine('Ping: ' + latency + 'ms');
     });
 
-    // Handle error.
     socket.on('connect_error', handleDisconnect);
     socket.on('disconnect', handleDisconnect);
 
-    // Handle connection.
     socket.on('welcome', function (playerSettings, gameSizes) {
         player = playerSettings;
         player.name = global.playerName;
@@ -192,9 +185,6 @@ function setupSocket(socket) {
 
     socket.on('playerDied', (data) => {
         const player = isUnnamedCell(data.playerEatenName) ? 'An unnamed cell' : data.playerEatenName;
-        //const killer = isUnnamedCell(data.playerWhoAtePlayerName) ? 'An unnamed cell' : data.playerWhoAtePlayerName;
-
-        //window.chat.addSystemLine('{GAME} - <b>' + (player) + '</b> was eaten by <b>' + (killer) + '</b>');
         window.chat.addSystemLine('{GAME} - <b>' + (player) + '</b> was eaten');
     });
 
@@ -223,7 +213,6 @@ function setupSocket(socket) {
                     status += (i + 1) + '. An unnamed cell';
             }
         }
-        //status += '<br />Players: ' + data.players;
         document.getElementById('status').innerHTML = status;
     });
 
@@ -231,12 +220,10 @@ function setupSocket(socket) {
         window.chat.addSystemLine(data);
     });
 
-    // Chat.
     socket.on('serverSendPlayerChat', function (data) {
         window.chat.addChatLine(data.sender, data.message, false);
     });
 
-    // Handle movement.
     socket.on('serverTellPlayerMove', function (playerData, userData, foodsList, massList, virusList) {
         if (global.playerType == 'player') {
             player.x = playerData.x;
@@ -251,129 +238,162 @@ function setupSocket(socket) {
         fireFood = massList;
     });
 
-    // Death.
     socket.on('RIP', function () {
         global.gameStart = false;
         render.drawErrorMessage('You died!', graph, global.screen);
-        window.setTimeout(() => {
-            document.getElementById('gameAreaWrapper').style.opacity = 0;
-            document.getElementById('startMenuWrapper').style.maxHeight = '1000px';
-            if (global.animLoopHandle) {
-                window.cancelAnimationFrame(global.animLoopHandle);
-                global.animLoopHandle = undefined;
-            }
-        }, 2500);
-    });
 
-    socket.on('kick', function (reason) {
-        global.gameStart = false;
-        global.kicked = true;
-        if (reason !== '') {
-            render.drawErrorMessage('You were kicked for: ' + reason, graph, global.screen);
-        }
-        else {
-            render.drawErrorMessage('You were kicked!', graph, global.screen);
-        }
-        socket.close();
-    });
+        // Get the player's position in the leaderboard
+        const position = leaderboard.findIndex(entry => entry.id === player.id) + 1;
+        const betAmount = 1; // Example: Replace with the actual bet amount for this match
+        const matchId = global.matchId || 'unknown'; // Replace with actual match ID if available
+
+        // Trigger the match over pop-up
+showMatchOverPopup(position, betAmount, matchId);
+
+window.setTimeout(() => {
+    document.getElementById('gameAreaWrapper').style.opacity = 0;
+    document.getElementById('startMenuWrapper').style.maxHeight = '1000px';
+    if (global.animLoopHandle) {
+        window.cancelAnimationFrame(global.animLoopHandle);
+        global.animLoopHandle = undefined;
+    }
+}, 2500);
+});
+
+socket.on('kick', function (reason) {
+global.gameStart = false;
+global.kicked = true;
+if (reason !== '') {
+    render.drawErrorMessage('You were kicked for: ' + reason, graph, global.screen);
+} else {
+    render.drawErrorMessage('You were kicked!', graph, global.screen);
+}
+socket.close();
+});
 }
 
 const isUnnamedCell = (name) => name.length < 1;
 
 const getPosition = (entity, player, screen) => {
-    return {
-        x: entity.x - player.x + screen.width / 2,
-        y: entity.y - player.y + screen.height / 2
-    }
-}
+return {
+x: entity.x - player.x + screen.width / 2,
+y: entity.y - player.y + screen.height / 2
+};
+};
 
 window.requestAnimFrame = (function () {
-    return window.requestAnimationFrame ||
-        window.webkitRequestAnimationFrame ||
-        window.mozRequestAnimationFrame ||
-        window.msRequestAnimationFrame ||
-        function (callback) {
-            window.setTimeout(callback, 1000 / 60);
-        };
+return window.requestAnimationFrame ||
+window.webkitRequestAnimationFrame ||
+window.mozRequestAnimationFrame ||
+window.msRequestAnimationFrame ||
+function (callback) {
+    window.setTimeout(callback, 1000 / 60);
+};
 })();
 
 window.cancelAnimFrame = (function (handle) {
-    return window.cancelAnimationFrame ||
-        window.mozCancelAnimationFrame;
+return window.cancelAnimationFrame ||
+window.mozCancelAnimationFrame;
 })();
 
 function animloop() {
-    global.animLoopHandle = window.requestAnimFrame(animloop);
-    gameLoop();
+global.animLoopHandle = window.requestAnimFrame(animloop);
+gameLoop();
 }
 
 function gameLoop() {
-    if (global.gameStart) {
-        graph.fillStyle = global.backgroundColor;
-        graph.fillRect(0, 0, global.screen.width, global.screen.height);
+if (global.gameStart) {
+graph.fillStyle = global.backgroundColor;
+graph.fillRect(0, 0, global.screen.width, global.screen.height);
 
-        render.drawGrid(global, player, global.screen, graph);
-        foods.forEach(food => {
-            let position = getPosition(food, player, global.screen);
-            render.drawFood(position, food, graph);
+render.drawGrid(global, player, global.screen, graph);
+foods.forEach(food => {
+    let position = getPosition(food, player, global.screen);
+    render.drawFood(position, food, graph);
+});
+fireFood.forEach(fireFood => {
+    let position = getPosition(fireFood, player, global.screen);
+    render.drawFireFood(position, fireFood, playerConfig, graph);
+});
+viruses.forEach(virus => {
+    let position = getPosition(virus, player, global.screen);
+    render.drawVirus(position, virus, graph);
+});
+
+let borders = { // Position of the borders on the screen
+    left: global.screen.width / 2 - player.x,
+    right: global.screen.width / 2 + global.game.width - player.x,
+    top: global.screen.height / 2 - player.y,
+    bottom: global.screen.height / 2 + global.game.height - player.y
+};
+if (global.borderDraw) {
+    render.drawBorder(borders, graph);
+}
+
+var cellsToDraw = [];
+for (var i = 0; i < users.length; i++) {
+    let color = 'hsl(' + users[i].hue + ', 100%, 50%)';
+    let borderColor = 'hsl(' + users[i].hue + ', 100%, 45%)';
+    for (var j = 0; j < users[i].cells.length; j++) {
+        cellsToDraw.push({
+            color: color,
+            borderColor: borderColor,
+            mass: users[i].cells[j].mass,
+            name: users[i].name,
+            radius: users[i].cells[j].radius,
+            x: users[i].cells[j].x - player.x + global.screen.width / 2,
+            y: users[i].cells[j].y - player.y + global.screen.height / 2
         });
-        fireFood.forEach(fireFood => {
-            let position = getPosition(fireFood, player, global.screen);
-            render.drawFireFood(position, fireFood, playerConfig, graph);
-        });
-        viruses.forEach(virus => {
-            let position = getPosition(virus, player, global.screen);
-            render.drawVirus(position, virus, graph);
-        });
-
-
-        let borders = { // Position of the borders on the screen
-            left: global.screen.width / 2 - player.x,
-            right: global.screen.width / 2 + global.game.width - player.x,
-            top: global.screen.height / 2 - player.y,
-            bottom: global.screen.height / 2 + global.game.height - player.y
-        }
-        if (global.borderDraw) {
-            render.drawBorder(borders, graph);
-        }
-
-        var cellsToDraw = [];
-        for (var i = 0; i < users.length; i++) {
-            let color = 'hsl(' + users[i].hue + ', 100%, 50%)';
-            let borderColor = 'hsl(' + users[i].hue + ', 100%, 45%)';
-            for (var j = 0; j < users[i].cells.length; j++) {
-                cellsToDraw.push({
-                    color: color,
-                    borderColor: borderColor,
-                    mass: users[i].cells[j].mass,
-                    name: users[i].name,
-                    radius: users[i].cells[j].radius,
-                    x: users[i].cells[j].x - player.x + global.screen.width / 2,
-                    y: users[i].cells[j].y - player.y + global.screen.height / 2
-                });
-            }
-        }
-        cellsToDraw.sort(function (obj1, obj2) {
-            return obj1.mass - obj2.mass;
-        });
-        render.drawCells(cellsToDraw, playerConfig, global.toggleMassState, borders, graph);
-
-        socket.emit('0', window.canvas.target); // playerSendTarget "Heartbeat".
     }
+}
+cellsToDraw.sort(function (obj1, obj2) {
+    return obj1.mass - obj2.mass;
+});
+render.drawCells(cellsToDraw, playerConfig, global.toggleMassState, borders, graph);
+
+socket.emit('0', window.canvas.target); // playerSendTarget "Heartbeat".
+}
 }
 
 window.addEventListener('resize', resize);
 
 function resize() {
-    if (!socket) return;
+if (!socket) return;
 
-    player.screenWidth = c.width = global.screen.width = global.playerType == 'player' ? window.innerWidth : global.game.width;
-    player.screenHeight = c.height = global.screen.height = global.playerType == 'player' ? window.innerHeight : global.game.height;
+player.screenWidth = c.width = global.screen.width = global.playerType == 'player' ? window.innerWidth : global.game.width;
+player.screenHeight = c.height = global.screen.height = global.playerType == 'player' ? window.innerHeight : global.game.height;
 
-    if (global.playerType == 'spectator') {
-        player.x = global.game.width / 2;
-        player.y = global.game.height / 2;
-    }
+if (global.playerType == 'spectator') {
+player.x = global.game.width / 2;
+player.y = global.game.height / 2;
+}
 
-    socket.emit('windowResized', { screenWidth: global.screen.width, screenHeight: global.screen.height });
+socket.emit('windowResized', { screenWidth: global.screen.width, screenHeight: global.screen.height });
+}
+
+/** Pop-up Functions **/
+
+function showMatchOverPopup(position, betAmount, matchId) {
+const popup = document.getElementById('matchOverPopup');
+document.getElementById('leaderboardPosition').innerText = `Your Position: ${position}`;
+document.getElementById('betAmount').innerText = `Bet Amount: ${betAmount} USDC`;
+document.getElementById('wonAmount').innerText = `You Won: ${calculateWinnings(position, betAmount)} USDC`;
+
+// Display the popup
+popup.style.visibility = 'visible';
+popup.style.opacity = '1';
+}
+
+function closePopup() {
+const popup = document.getElementById('matchOverPopup');
+popup.style.visibility = 'hidden';
+popup.style.opacity = '0';
+}
+
+function calculateWinnings(position, betAmount) {
+// Example logic: you can customize this based on how winnings are calculated
+if (position === 1) return betAmount * 2; // First place gets double the bet
+if (position === 2) return betAmount * 1.5; // Second place gets 1.5x the bet
+if (position === 3) return betAmount; // Third place gets their bet amount back
+return 0; // Others get nothing
 }
